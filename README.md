9f58e1dd53c2458a1fcafa8174c611e67008ce58
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2024-10-01 14:05:05 +0900

    initialize repository with aws-amplify images.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Nuxt3 AWS Amplify(with PostgreSQL, Drizzle) Template
+
+License: Apache 2.0 license
+
+## How to start with nuxi
+
+```shell
+nuxi init -t gh:dalbodeule/nuxt3-aws
+```
+
+## Libraries
+- Nuxt3
+- Vue3
+- @Nuxthub/core
+- Drizzle
---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-08-22',
+  devtools: { enabled: true },
+  nitro: {
+    preset: 'aws_amplify',
+    serveStatic: true,
+    output: {
+      publicDir: './output/server'
+    }
+  },
+  css: [
+    "@/assets/css/main.css"
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  },
+  runtimeConfig: {
+    aws: {
+      profile: process.env.AWS_PROFILE,
+      secretArn: process.env.AWS_SECRET,
+      resourceArn: process.env.AWS_RESOURCE,
+    },
+  },
+  modules: ["@nuxtjs/turnstile", "@nuxt/eslint"]
+})
